92f1edda684fbfbd3970561a6171cae4fbfe974c
doanhoang1293 <doanhv1293@gmail.com>
2015-11-27 10:59:05 +0700

    cập nhật bài tập lớn
    File báo cáo bài tập lớn

---
+++ src/Client.java
@@ -0,0 +1,153 @@
+package hoangdoan.me.pingo;
+
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+/*
+ * The Client that can be run both as a console or a GUI
+ */
+public class Client  {
+
+	// for I/O
+	private ObjectInputStream sInput;		// to read from the socket
+	private ObjectOutputStream sOutput;		// to write on the socket
+	private Socket socket;
+
+	// if I use a GUI or not
+	private ClientGUI cg;
+	
+	// the server, the port and the username
+	private String server, username;
+	private int port;
+
+
+	Client(String server, int port, String username, ClientGUI cg) {
+		this.server = server;
+		this.port = port;
+		this.username = username;
+		// save if we are in GUI mode or not
+		this.cg = cg;
+	}
+	
+	
+	public boolean start() {
+		// try to connect to the server
+		try {
+			socket = new Socket(server, port);
+		} 
+		// if it failed not much I can so
+		catch(Exception ec) {
+			display("Error connectiong to server:" + ec);
+			return false;
+		}
+		
+		String msg = "Connection accepted " + socket.getInetAddress() + ":" + socket.getPort();
+		display(msg);
+	
+		/* Creating both Data Stream */
+		try
+		{
+			sInput  = new ObjectInputStream(socket.getInputStream());
+			sOutput = new ObjectOutputStream(socket.getOutputStream());
+		}
+		catch (IOException eIO) {
+			display("Exception creating new Input/output Streams: " + eIO);
+			return false;
+		}
+
+		// creates the Thread to listen from the server 
+		new ListenFromServer().start();;
+		// Send our username to the server this is the only message that we
+		// will send as a String. All other messages will be ChatMessage objects
+		try
+		{
+			sOutput.writeObject(username);
+		}
+		catch (IOException eIO) {
+			display("Exception doing login : " + eIO);
+			//disconnect();
+			return false;
+		}
+		// success we inform the caller that it worked
+		return true;
+	}
+
+	/*
+	 * To send a message to the the GUI
+	 */
+	private void display(String msg) {
+		
+			cg.append(msg + "\n");		// append to the ClientGUI JTextArea (or whatever)
+	}
+	
+	/*
+	 * To send a message to the server
+	 */
+	void sendMessage(ChatMessage msg) {
+		try {
+			sOutput.writeObject(msg);
+		}
+		catch(IOException e) {
+			display("Exception writing to server: " + e);
+		}
+	}
+
+	/*
+	 * When something goes wrong
+	 * Close the Input/Output streams and disconnect not much to do in the catch clause
+	 */
+	void disconnect() {
+		try { 
+			if(sInput != null) sInput.close();
+		}
+		catch(Exception e) {} // not much else I can do
+		try {
+			if(sOutput != null) sOutput.close();
+		}
+		catch(Exception e) {} // not much else I can do
+        try{
+			if(socket != null) socket.close();
+		}
+		catch(Exception e) {} // not much else I can do	
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+	/*
+	 * a class that waits for the message from the server and append them to the JTextArea
+	 * if we have a GUI or simply System.out.println() it in console mode
+	 */
+	class ListenFromServer extends Thread {
+
+		public void run() {
+			while(true) {
+				try {
+					ChatMessage msg = (ChatMessage) sInput.readObject();
+					if (msg.getType() == ChatMessage.MESSAGE){
+						cg.append(msg.getMessage());
+					}
+					if (msg.getType() == ChatMessage.START)
+						cg.GameEnable();
+					if (msg.getType() == ChatMessage.STOP)
+						cg.GameDisable();
+					if (msg.getType() == ChatMessage.NUMBER){
+						cg.ButtonSelected(msg.getMessage());
+						cg.append(username+": "+msg.getMessage());
+					}
+				}
+				catch(IOException e) {
+					display("Server has close the connection: " + e); 	
+					break;
+				}
+				// can't happen with a String object but need the catch anyhow
+				catch(ClassNotFoundException e2) {
+				}
+			}
+		}
+	}
+}
+
